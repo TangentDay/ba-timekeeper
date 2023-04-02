764f435c93721fa9d3c8d0031121cd247c82d892
TangentDay <stick.sugar.syndrome@gmail.com>
2023-04-02 19:15:11 +0900

    msg box

---
+++ ba-timekeeper/MsgBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ba_timekeeper
+{
+    internal class MsgBox
+    {
+        public static void ShowErr(string msg)
+        {
+            MessageBox.Show(msg, "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
